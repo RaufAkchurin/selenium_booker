138c64f520dd1922122a73dd304da7caacf29322
rauf.akchurin <abdurauf.dev@gmail.com>
2023-11-01 11:46:04 +0500

    README added

---
+++ Active/inactive/disabled.py
@@ -0,0 +1,49 @@
+# SELECTED empty slot
+
+< div
+id = "lyt_slot_clone_17"
+data - id = "lyt_slot_clone_17"
+data - layout = "false"
+data - is -form = "false"
+data - component = "true"
+data - slot = "children"
+data - slot - type = "vertical"
+data - slot - spacing = "2"
+data - slot - content = "true"
+
+
+class ="_1SfRmVRt lyt_lyt81691592347152 _3ZFeUWI4 _3n1MFwi7 slotActive _1KWhrReL" style="display: flex; flex-direction: column; justify-content: center; box-sizing: border-box; align-items: center; padding: 4px 8px; position: relative; flex-shrink: 0; overflow: hidden;" > < div data-id="lbl_limit" data-layout="false" data- is -form="false" data-component="true" class ="_1SfRmVRt lbl_lbl51691592376684  label bold bold" style="color: rgb(7, 106, 215); display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;" > < span > 11 / 12 < /span > < /div > < div style="flex-shrink: 0; height: 2px;" data-snap-ignore="true" > < /div > < div data-id="lbl_slot" data-layout="false" data- is -form="false" data-component="true" class ="_1SfRmVRt lbl_lbl51691592376684  label semibold semibold" style="display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;" > < span > 17:00 - 18: 00 <
+
+/ span > < / div > < / div >
+
+# EMPTY SLOT NOT SELECTED
+
+< div
+id = "lyt_slot_clone_8"
+data - id = "lyt_slot_clone_8"
+data - layout = "false"
+data - is -form = "false"
+data - component = "true"
+data - slot = "children"
+data - slot - type = "vertical"
+data - slot - spacing = "2"
+data - slot - content = "true"
+
+
+class ="_1SfRmVRt lyt_lyt81691592347152 _3ZFeUWI4 _3n1MFwi7 slotInactive _1KWhrReL" style="display: flex; flex-direction: column; justify-content: center; box-sizing: border-box; align-items: center; padding: 4px 8px; position: relative; flex-shrink: 0; overflow: hidden;" > < div data-id="lbl_limit" data-layout="false" data- is -form="false" data-component="true" class ="_1SfRmVRt lbl_lbl51691592376684  label bold bold" style="color: rgb(7, 106, 215); display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;" > < span > 10 / 12 < /span > < /div > < div style="flex-shrink: 0; height: 2px;" data-snap-ignore="true" > < /div > < div data-id="lbl_slot" data-layout="false" data- is -form="false" data-component="true" class ="_1SfRmVRt lbl_lbl51691592376684  label semibold semibold" style="display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;" > < span > 08:00 - 09: 00 < / span > < / div > < / div >
+
+
+# BOOKED SLOT
+
+<div
+id="lyt_slot_clone_13"
+data-id="lyt_slot_clone_13"
+data-layout="false"
+data-is-form="false"
+data-component="true"
+data-slot="children"
+data-slot-type="vertical"
+data-slot-spacing="2"
+data-slot-content="true"
+
+class="_1SfRmVRt lyt_lyt81691592347152 _3ZFeUWI4 _3n1MFwi7 slotDisabled _1KWhrReL" style="display: flex; flex-direction: column; justify-content: center; box-sizing: border-box; align-items: center; padding: 4px 8px; position: relative; flex-shrink: 0; overflow: hidden;"><div data-id="lbl_limit" data-layout="false" data-is-form="false" data-component="true" class="_1SfRmVRt lbl_lbl51691592376684  label bold _2ntznnGK bold" style="color: rgb(7, 106, 215); display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;"><span>12 / 12</span></div><div style="flex-shrink: 0; height: 2px;" data-snap-ignore="true"></div><div data-id="lbl_slot" data-layout="false" data-is-form="false" data-component="true" class="_1SfRmVRt lbl_lbl51691592376684  label semibold _2ntznnGK semibold" style="display: flex; position: relative; flex-shrink: 0; height: auto; width: auto;"><span>13:00-14:00</span></div></div>
--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def find_by_xpath(xpath: str):
     return WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
 
 
+def select_first_in_dropdown(xpath: str):
+    btn = find_by_xpath(xpath)
+    btn.send_keys(Keys.ARROW_DOWN)
+    btn.send_keys(Keys.ARROW_DOWN)
+    btn.send_keys(Keys.ENTER)
+
+
 def logging_by_email(login: str, password: str):
     login_window = find_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/div/div[2]/div/div[4]/div[2]/div/div/div[2]/div[3]/div/div/input")
     login_window.send_keys(login)
@@ -43,7 +50,8 @@ password = "Fufik_777176"
 
 logging_by_email(login=login, password=password)
 
-#CREATE_REQUEST
+# CREATE_REQUEST_WINDOW
+
 create_request_btn = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[1]/div[2]/div/div[4]/div[3]/button/div/span")
 time.sleep(1)
 create_request_btn.click()
@@ -60,11 +68,27 @@ SELECT_LOCATOR.send_keys(Keys.ENTER)
 next_page_btn = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div[2]/button/div/span")
 next_page_btn.click()
 
+transport_type_btn = select_first_in_dropdown("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[3]/div[3]/div/div")
+transport_kind_btn = select_first_in_dropdown("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[5]/div[3]/div/div")
+
+
 copy_data_btn = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[7]/div/label/span")
 copy_data_btn.click()
 
+
 next_page_btn2 = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div[2]/button/div/span")
 next_page_btn2.click()
+
+# DAY_BOOKING_PAGE
+window_for_slots = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[3]/div/div[1]/div/div[1]/div[3]/div/div[2]/svg")
+window_for_slots.click()
+
+# blok_slots_xpath = '//*[@id="lyt_slots"]'
+# slots = find_by_xpath(blok_slots_xpath)
+# slots.click()
+# for slot in slots:
+#     print(slot.getattr("class"))
+
 
 
 
