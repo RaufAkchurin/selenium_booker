4a1a4f50ac582ff394fadf33783fcfae7ea70283
rauf.akchurin <abdurauf.dev@gmail.com>
2023-11-02 15:53:29 +0500

    booking logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # 1) - 2 обучение
 # 2 день - 2 часа
 # 3 день - 3 часа
+# 4 день - 2 часа + 1
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,7 +17,7 @@ from selenium.webdriver.common.keys import Keys
 #DATA_FOR_BOOKING
 login = "joffre@ebyjeans.com"
 password = "Fufik_777176"
-# day = 5
+day = 5
 
 
 def find_by_xpath(xpath: str):
@@ -83,13 +85,74 @@ next_page_btn2 = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/
 next_page_btn2.click()
 
 # DAY_BOOKING_PAGE
-day = 2
-day_choice_window = find_by_xpath(
-    "/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[3]/div/div[1]/div/div[1]/div[3]/div/div[1]/div/input")
-day_choice_window.send_keys(day)
 
-block = browser.find_elements(By.XPATH, '//*[@id="lyt_slots"]')
-print(len(block))
+
+def day_choice(day: int):
+    day_choice_window = find_by_xpath(
+        "/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div/div[3]/div/div[3]/div/div[1]/div/div[1]/div[3]/div/div[1]/div/input")
+    day_choice_window.send_keys(day)
+
+
+day_choice(day)
+
+
+def inactive_checker(slot):
+    if "slotInactive" in slot.get_attribute("class"):
+        slot.click()
+        create_btn = find_by_xpath("/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/div[2]/button")
+        create_btn.click()
+        print("slotInactive")
+    if "slotDisabled" in slot.get_attribute("class"):
+        print("slotDisabled")
+
+
+def slots_booker():
+    while True:
+        all_slots = []
+        for i in range(1, 24):
+            try:
+                xpath = f'//*[@id="lyt_slot_clone_{str(i)}"]'
+                slot = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+                all_slots.append(slot)
+                inactive_checker(slot)
+            except:
+                break
+        print(len(all_slots))
+
+
+slots_booker()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if len(all_slots) > 0:
+#     for slot in all_slots:
+#         print(slot.get_attribute("id"))
+
+# slot1 = find_by_xpath('//*[@id="lyt_slot_clone_1"]')
+# slot2 = find_by_xpath('//*[@id="lyt_slot_clone_2"]')
+# slot3 = find_by_xpath('//*[@id="lyt_slot_clone_3"]')
+# slot4 = find_by_xpath('//*[@id="lyt_slot_clone_4"]')
+# #
+# #
+# # print(slot1.get_attribute("id"), slot1.get_attribute("class"))
+#
+# block = find_by_xpath('//*[@id="lyt_slots"]')
+# print(block.get_attribute("class"))
+
+
+
 
 
 
